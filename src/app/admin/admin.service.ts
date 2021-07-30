296b59bf1d13bd737db013c28d0bb8b63946011c
Kristine <kristine.gh99@gmail.com>
2021-07-30 14:32:10 +0400

    Fixing admin header, adding admin service

---
+++ src/app/admin/admin.service.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class AdminService {
+  constructor(private http: HttpClient) {}
+
+  getUser() {
+    return this.http.get(`/users`);
+  }
+}
