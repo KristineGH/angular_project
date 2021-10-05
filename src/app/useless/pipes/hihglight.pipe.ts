b09ad0d4a5b1aa2fa2c416a4e8108ac6d14869d8
VOLO-DC\kristine.gharibyan <kristine.gharibyan@volo.global>
2021-10-05 16:47:48 +0400

    select all letters in highlight pipe

--- src/app/useless/pipes/hihglight.pipe.ts
+++ src/app/useless/pipes/hihglight.pipe.ts
@@ -9,10 +9,13 @@ export class HighlightPipe implements PipeTransform {
         // debugger
         if (val && arg) {
             if (val.toUpperCase().indexOf(arg.toUpperCase()) != -1) {
+                var pattern = arg.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+                var regex = new RegExp(pattern, 'gi');
+
                 let start = val.toUpperCase().indexOf(arg.toUpperCase());
                 let filteredText = val.substr(start, arg.length );
                 console.log(filteredText)
-                return val.replace(filteredText, `<span class='yellow'>${filteredText}</span>`);
+                return val.replace(regex, (filteredText) => `<span class='yellow'>${filteredText}</span>`);
             }
         }
         return val;
